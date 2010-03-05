65034b8c175d37163b83a43576535fae009fbeff
Tom Drummond <tom@devioustree.co.uk>
2010-03-05 12:42:07 +0000

    Made django-follow generic so any type of object can be followed

---
+++ follow/util.py
@@ -0,0 +1,43 @@
+from django.db.models.fields.related import ManyToManyField, ForeignKey
+
+registry = []
+model_map = {}
+
+def followers_for_object(self):
+    from models import Follower
+    return Follow.objects.get_followers_for_object(self)
+
+def register(model, field_name = None, m2m = False):
+    """
+    This registers the model class so it can have followers
+    """
+    from models import Follow
+    if model in registry:
+        return
+        
+    registry.append(model)
+    
+    related_name = 'follow_%s' % model._meta.module_name
+    
+    if not field_name:
+        field_name = model._meta.module_name
+    
+    # Create foreignkeys by default - less sql queries for lookups
+    if m2m:
+        field = ManyToManyField(
+            model,
+            related_name = related_name,
+        )
+    else:
+        field = ForeignKey(
+            model,
+            related_name = related_name,
+            blank = True,
+            null = True,
+        )
+    
+    field.contribute_to_class(Follow, field_name)
+    setattr(model, 'followers', followers_for_object)
+    
+    # We need to keep track of which fields and which kind of fields point where
+    model_map[model] = [related_name, field_name, m2m]
