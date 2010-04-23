c8cb2d2cd105667f88b75162d2abd4d56a9fe519
Alen Mujezinovic <flashingpumpkin@gmail.com>
2010-04-23 16:25:16 +0100

    Added a template tag to return following links

---
+++ follow/templatetags/follow_tags.py
@@ -0,0 +1,46 @@
+from django import template
+from django.core.urlresolvers import reverse
+from follow.models import Follow
+
+register = template.Library()
+
+def _follow_link(object):
+    return reverse('follow', args=[object._meta.app_label, object._meta.object_name.lower(), object.pk])
+
+def _unfollow_link(object):
+    return reverse('unfollow', args=[object._meta.app_label, object._meta.object_name.lower(), object.pk])
+
+@register.tag
+def follow_link(parser, token):
+    """
+    Returns either a link to follow or to unfollow.
+    
+    Usage::
+        
+        {% follow_link object %}
+        
+    """
+    
+    try:
+        tag, obj = token.split_contents()
+    except:
+        raise template.TemplateSyntaxError("The ``follow_link`` tag requires exactly one argument.")
+    return FollowLinkNode(obj)
+
+class FollowLinkNode(template.Node):
+    def __init__(self, obj):
+        self.obj = template.Variable(obj)
+        
+    def render(self, context):
+        obj = self.obj.resolve(context)
+        
+        try:
+            request = context['request']
+        except KeyError:
+            raise template.TemplateSyntaxError('There is no request object in the template context.')
+        
+        if Follow.objects.is_user_following(request.user, obj):
+            return _unfollow_link(obj)
+        else:
+            return _follow_link(obj)
+        
--- follow/views.py
+++ follow/views.py
@@ -2,42 +2,42 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.db.models.loading import cache
 from django.http import (HttpResponse, HttpResponseBadRequest,
-    HttpResponseRedirect)
+    HttpResponseRedirect, HttpResponseServerError)
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils.translation import gettext as _
 from follow import follow as _follow, unfollow as _unfollow
 
 
-
 def check(func):
     """ 
     Check the permissions, http method and login state
     """
-    def iCheck(request, username):
-        try:
-            user = User.objects.get(username=username)
-        except User.DoesNotExist:
-            return HttpResponseBadRequest()
-        if user == request.user:
-            return HttpResponseBadRequest()
-        
-        func(request, user)
-        if request.is_ajax:
+    def iCheck(request, *args, **kwargs):
+        follow = func(request, *args, **kwargs)
+        if request.is_ajax():
             return HttpResponse('ok')
-        return HttpResponseRedirect(request.META['REFERER'])
+        try:
+            return HttpResponseRedirect(request.META['HTTP_REFERER'])
+        except KeyError:
+            try:
+                return HttpResponseRedirect(follow.get_object().get_absolute_url())
+            except AttributeError:
+                return HttpResponseServerError('"%s" object of type ``%s`` has no method ``get_absolute_url()``.' % (
+                    unicode(follow.get_object()), follow.get_object().__class__))
     return iCheck
 
-@login_required
-@check
-def follow(request, app, model, obj):
-    model = cache.get_model(app, model)
-    obj = model.objects.get(pk=obj)
-    _follow(request.user, obj)
 
 @login_required
 @check
-def unfollow(request, app, model, obj):
+def follow(request, app, model, id):
     model = cache.get_model(app, model)
-    obj = model.objects.get(pk=obj)
-    _unfollow(request.user, obj)
+    obj = model.objects.get(pk=id)
+    return _follow(request.user, obj)
+
+@login_required
+@check
+def unfollow(request, app, model, id):
+    model = cache.get_model(app, model)
+    obj = model.objects.get(pk=id)
+    return _unfollow(request.user, obj)
