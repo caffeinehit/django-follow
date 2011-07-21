3c5062652b32a34270a55e3b4540ff9ac276c33e
Alen Mujezinovic <flashingpumpkin@gmail.com>
2011-07-21 16:27:17 +0100

    Version bump. Well, templatetags should be included too

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 setup(
     name='django-follow',
     description='Application which enables following features for users. Can be used for contact books or whatnot',
     long_description=open('README.rst').read(),
-    packages=['follow'],
+    packages=['follow', 'follow.templatetags'],
     author='Alen Mujezinovic',
     author_email='alen@caffeinehit.com',
     url='https://github.com/caffeinehit/django-follow',
-    version='0.3',
+    version='0.4',
 )
